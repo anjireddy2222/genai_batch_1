14171364db5303e8c33bf494158fc4409c0bb991
Anji Reddy <akkalaanjireddy@gmail.com>
2026-04-28 21:07:17 +0530

    28-04 class

---
+++ backend/app.py
@@ -0,0 +1,59 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
+
+# run fastapi project:  uvicorn app:app --reload  
+
+# app -> file name -> app.py
+# app: fastapi object
+
+# receive data -> connect with ai -> work flow -> return response
+
+# create api -> for every task
+
+# method -> api path -> data
+
+@app.get("/")
+def index():
+    print("server running")
+    return { "result": "success", "data": {}, "message": "Server running" }
+
+
+@app.get("/get-llms") # api path
+def get_llms():
+    return { "result": "success", "message": "ok", "data": ["claude", "openai"]}
+
+# courses, expereince, country
+
+# name, email, password
+
+class Signup(BaseModel):
+    name: str
+    email: str
+    password: str
+
+@app.post("/signup")
+def signup(signup: Signup):
+    return { "result": "success", "message": "account created succesfully", "data": signup }
+
+
+class ChatData(BaseModel):
+    business_id: str
+    conversation_id: str
+    message: str
+    session_id: str
+    timezone: str
+
+
+@app.post("/api/wa/chat")
+def ai_chat(chat_data: ChatData):
+    print( chat_data.business_id)
+    return { "result": "success", 'message': "our team will get back to you", "data": chat_data }
+
+
+
+
---
+++ system_28_04.md
@@ -0,0 +1,25 @@
+
+UI -> ReactJS, HTML,
+
+
+Current project Backend -> Nodejs, java, python, .net
+
+
+
+AI application -> Python -> Django, Flask, FastAPI -> 
+
+AI -> FastAPI
+
+
+backend setup -> server running -> install external packages install -> POST, GET -> DB -> OpenAI integration
+
+# install: pip install fastapi uvicorn
+# cd project folder name
+# run python project:  uvicorn app:app --reload  
+
+app -> file name -> app.py
+app: fastapi object
+
+# uvicorn app:app --reload --port 3000
+
+
